4d1879f24decd2135b611446252261a3ea97c667
xray-robot <robot@xray.app>
2026-07-17 14:02:10 +0300

    Initial commit

--- packages/xray-mini-app-sdk-cip30/src/index.ts
+++
@@ -1,2 +0,0 @@
-export * as miniAppCip30Client from "./client"
-export * as miniAppCip30Host from "./host"
--- packages/xray-mini-app-sdk/src/index.ts
+++
@@ -1,2 +0,0 @@
-export * as miniAppClient from "./client"
-export * as miniAppHost from "./host"
